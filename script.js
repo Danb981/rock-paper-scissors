ffb892557b471b344971ae31d4ce79a6bd476a13
Daniel Burgess <37884090+Danb981@users.noreply.github.com>
2022-10-31 13:10:01 -0400

    Function playing a round made

--- script.js
+++ script.js
@@ -10,3 +10,34 @@ function getComputerChoice(){
         return "Scissors";
     }
 }
+
+function playRound(playerSelection, getComputerChoice){
+    playerSelection = playerSelection.toUpperCase();
+    computerSelection = computerSelection.toUpperCase();
+    let playerWin = false; 
+    let isDraw = false;
+
+    if(playerSelection == computerSelection){ //draw condition
+        isDraw = true;
+    }
+    else if(playerSelection == 'ROCK' && computerSelection == 'SCISSORS'){ //else ifs represent player win conditions, if none are met the computer must have won
+        playerWin = true;
+    }
+    else if(playerSelection == 'PAPER' && computerSelection == 'ROCK'){
+        playerWin = true;
+    }
+    else if(playerSelection == 'SCISSORS' && computerSelection == 'PAPER'){
+        playerWin = true;
+    }
+
+    if(isDraw){
+        return `Draw! ${playerSelection} negates ${computerSelection}`;
+    }
+    else if(playerWin){
+        return `You Win! ${playerSelection} beats ${computerSelection}`;
+    }
+    else{
+        return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    }
+}
+
