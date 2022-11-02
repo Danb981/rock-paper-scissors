d805eea1449bb100c836875ce0dd693ddae0b700
Daniel Burgess <37884090+Danb981@users.noreply.github.com>
2022-11-02 13:58:39 -0400

    Added GUI

--- script.js
+++ script.js
@@ -11,11 +11,12 @@ function getComputerChoice(){
     }
 }
 
-function playRound(){
-    let playerSelection = prompt("Rock, Paper, or Scissors?").toUpperCase();
+function playRound(event){
+    let playerSelection = event.target.id.toUpperCase();
     let computerSelection = getComputerChoice().toUpperCase();
     let playerWin = false; 
     let isDraw = false;
+    let roundResultText;
 
     if(playerSelection == computerSelection){ //draw condition
         isDraw = true;
@@ -31,19 +32,41 @@ function playRound(){
     }
 
     if(isDraw){
-        console.log(`Draw this round! ${playerSelection} negates ${computerSelection}`);
-        return 0;
+        roundResultText=`Draw this round! ${playerSelection} negates ${computerSelection}`;
     }
     else if(playerWin){
-        console.log(`You win this round! ${playerSelection} beats ${computerSelection}`);
-        return 1;
+        roundResultText=`You win this round! ${playerSelection} beats ${computerSelection}`;
+        playerScore++;
     }
     else{
-        console.log(`You lose this round! ${computerSelection} beats ${playerSelection}`);
-        return 2;
+        roundResultText=`You lose this round! ${computerSelection} beats ${playerSelection}`;
+        computerScore++;
+    }
+
+    updateUI(roundResultText);
+}
+
+function updateUI(roundResultText){
+    roundResult.textContent = roundResultText;
+    scores.textContent = `Player: ${playerScore} Computer: ${computerScore}`;
+    if(playerScore === 5 || computerScore === 5){
+        winner.textContent = `${playerScore === 5 ? 'You win' : 'Computer wins'}!`;
+        reset.style.visibility = 'visible';
+        buttons.forEach(button => button.style.visibility="hidden");
     }
 }
 
+function resetGame(event){
+    playerScore = 0;
+    computerScore = 0;
+    roundResult.textContent = "";
+    scores.textContent = "";
+    winner.textContent = "";
+    reset.style.visibility = 'hidden';
+    buttons.forEach(button => button.style.visibility="visible");
+}
+
+/* code playing 5 rounds
 function game(){
     let playerScore = 0;
     let computerScore = 0;
@@ -67,6 +90,25 @@ function game(){
         console.log(`You lose the game! ${computerScore} rounds to ${playerScore} rounds`);
     }
 }
+*/
 
-game();
+let playerScore=0;
+let computerScore=0;
+
+const resultsContainer = document.querySelector(".resultsContainer");
+const roundResult = resultsContainer.querySelector("#roundResult");
+const scores = resultsContainer.querySelector("#scores");
+const winner = resultsContainer.querySelector("#winner");
+const reset = resultsContainer.querySelector("#reset");
+reset.style.visibility = 'hidden';
+reset.addEventListener('click', resetGame);
+
+const buttonContainer = document.querySelector(".buttonContainer");
+const buttons = buttonContainer.querySelectorAll("button");
+buttons.forEach(button => {
+    button.addEventListener('click', playRound)
+});
+
+//game();
+
 
